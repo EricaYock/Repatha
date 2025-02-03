6738a355e64cc3c0a0a6268dfd69c41a464921a6
Erica Yock <yock@adobe.com>
2025-02-03 08:39:30 -0800

    changed publish url in content frag block

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -1,5 +1,5 @@
 // put your AEM publish address here
-const AEM_HOST = 'https://publish-p99452-e916802.adobeaemcloud.com';
+const AEM_HOST = 'https://publish-p134711-e1334063.adobeaemcloud.com';
 
 export default function decorate(block) {
   const slugDiv = block.querySelector('div:nth-child(1)');
