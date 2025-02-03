2175eb70495f7aa16a1ee182aefd6b3e4237d2e3
Erica Yock <yock@adobe.com>
2025-02-03 11:54:23 -0800

    edited cf

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -18,26 +18,10 @@ export default function decorate(block) {
   fetch('${AEM_HOST}/graphql/execute.json/repatha/repatha;slug=${slug}')
     .then((response) => response.json())
     .then((response) => {
-      const backgroundImage = response.data.adventureList.items[0].primaryImage._path;
-      document.getElementById(adventureDiv.id).innerHTML = `<section><img src=${AEM_HOST}${backgroundImage}></section>`;
 
-      const adventureTitle = response.data.adventureList.items[0].title;
-      document.getElementById(adventureDiv.id).innerHTML += `<section><h3>${adventureTitle}</h3></section>`;
+      const repatha = response.data.importantSafetyInformationList.items[0].importantSafetyInformation;
+      document.getElementById(adventureDiv.id).innerHTML += `<section><h3>${repatha}</h3></section>`;
 
-      const adventureDesc = response.data.adventureList.items[0].description.plaintext;
-      document.getElementById(adventureDiv.id).innerHTML += `<section>${adventureDesc}</section>`;
-
-      const { adventureType } = response.data.adventureList.items[0];
-      document.getElementById(adventureDiv.id).innerHTML += '<section>' + `Adventure Type: ${adventureType}</section>`;
-
-      const { tripLength } = response.data.adventureList.items[0];
-      document.getElementById(adventureDiv.id).innerHTML += '<section>' + `Trip Length: ${tripLength}</section>`;
-
-      const tripDifficulty = response.data.adventureList.items[0].difficulty;
-      document.getElementById(adventureDiv.id).innerHTML += '<section>' + `Difficulty: ${tripDifficulty}</section>`;
-
-      const { groupSize } = response.data.adventureList.items[0];
-      document.getElementById(adventureDiv.id).innerHTML += '<section>' + `Group Size: ${groupSize}</section>`;
       /*
 const tripItinerary= response.data.adventureList.items[0].itinerary.html;
 document.getElementById(adventureDiv.id).innerHTML += "<section>" + "Itinerary: </br>" + tripItinerary + "</section>";
